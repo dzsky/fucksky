93dbe24b699e22d7b3b70591758654c1c0dcfe1d
songwanfu <15623233011@163.com>
2015-07-26 20:42:58 +0800

    v1.1

---
+++ file.php
@@ -0,0 +1,3 @@
+﻿<?php
+	echo $_FILES['file']["name"];
+?>
---
+++ upload.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+	$fileName= $_FILES['file']['name'];
+
+	// // $data = array('error' => "上传失败");
+	// $data = "1";
+	// $data1 = "0";
+	// if($fileName == ''){
+	// 	echo json_encode($data);
+	// }
+	// else{
+	// 	echo json_encode($data1);
+	// }
+	// 
+	
+	// $data = array(
+	// 	'initialPreview' => "<img src='images/movie-1.jpg' class='file-preview-image' alt='1' title='1'>",
+	// 	);
+	// echo json_encode($data);
+	// 
+	$data = array(
+		'name' => $fileName,
+		);
+
+	echo json_encode($data);
+?>
